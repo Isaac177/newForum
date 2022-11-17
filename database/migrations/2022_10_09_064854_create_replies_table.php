0e9cfcf6db88771a30e892c3cf447c89781ac734
Isaac177 <isaacjogues177@gmail.com>
2022-11-17 21:53:48 +0600

    Reply Model

--- database/migrations/2022_10_09_064854_create_replies_table.php
+++ database/migrations/2022_10_09_064854_create_replies_table.php
@@ -6,24 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateRepliesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->id();
+            $table->text('body');
+            $table->integer('replyAble_id');
+            $table->string('replyAble_type')->default('threads');
+            $table->foreignId('author_id')->constrained('users'); // This is the author of the reply
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('replies');
