7de4c35c8885ac6a2149b318ec631d0ed57a8d0a
Isaac177 <isaacjogues177@gmail.com>
2022-12-16 14:59:08 +0600

    Notif components

---
+++ resources/views/livewire/notifications/indicator.blade.php
@@ -0,0 +1,15 @@
+<div class="relative w-8">
+    <a href="#" class="relative flex items-center justify-center w-6 h-6">
+        <div class="relative flex items-center justify-center w-6 h-6">
+            <span class="absolute inline-flex w-full h-full bg-blue-400 rounded-full opacity-75 animate-ping"> </span>
+                <span class="relative inline-flew w-4 h-4 text-blue-400 rounded-full">
+                    <x-zondicon-notifications-outline />
+                </span>
+        </div>
+
+        <span class="absolute inline-flex items-center justify-center w-3 h-3 text-xs font-bold text-white bg-red-500 rounded-full -top-1 -right-1">
+            <livewire:notifications.count />
+        </span>
+
+    </a>
+</div>
